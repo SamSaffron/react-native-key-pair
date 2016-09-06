adce1ae7a3aa71a4e904097425b42cfccb8d3b62
Sam Saffron <sam.saffron@gmail.com>
2016-09-06 11:26:40 +1000

    more implementation

--- android/src/main/java/com/RNKeyPair/RNKeyPairModule.java
+++ android/src/main/java/com/RNKeyPair/RNKeyPairModule.java
@@ -1,13 +1,23 @@
 
 package com.RNKeyPair;
 
+import android.util.Base64;
+
+import com.facebook.react.bridge.NoSuchKeyException;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableNativeMap;
 
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
+import java.security.spec.X509EncodedKeySpec;
 
 public class RNKeyPairModule extends ReactContextBaseJavaModule {
 
@@ -24,10 +34,32 @@ public class RNKeyPairModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void generate(Callback callback) {
+  public void generate(Callback callback)  {
     WritableNativeMap keys = new WritableNativeMap();
-    keys.putString("public","123");
-    keys.putString("private","345");
+
+    try {
+
+      KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+      kpg.initialize(2048);
+      KeyPair keyPair = kpg.genKeyPair();
+      byte[] publicKey = keyPair.getPublic().getEncoded();
+      byte[] privateKey = keyPair.getPrivate().getEncoded();
+
+      X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(publicKey);
+      KeyFactory keyFact = KeyFactory.getInstance("RSA", "BC");
+      String keyString = Base64.encodeToString(keyFact.generatePublic(x509KeySpec).getEncoded(),0);
+
+      keys.putString("public", keyString);
+      keys.putString("private", "345");
+    }
+    catch(InvalidKeySpecException e){
+      // don't care
+    }
+    catch(NoSuchAlgorithmException e) {
+    }
+    catch(NoSuchProviderException e) {
+    }
+
     callback.invoke(keys);
   }
 }
